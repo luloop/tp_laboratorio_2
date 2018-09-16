c45829bcd15e7b18d931996dec38859fa5af9257
luloop <lucilarizzi@gmail.com>
2018-09-16 10:33:50 -0300

    clase numero, lista

---
+++ TP1/Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        #region Metodos
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            /*  realizará la operación pedida entre ambos números */
+            return 0;
+        }
+
+        private static string ValidarOperador( string operador)
+        {
+            // Deberá validar que el operador
+            // recibido sea +, -, / o *.Caso contrario retornará +.
+            return "0";
+        }
+
+        #endregion
+
+    }
+}
---
+++ TP1/Entidades/Entidades/Numero.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double _Numero;
+
+    #region Propiedades
+
+        public string SetNumero {                
+            set
+            {
+                this._Numero = ValidarNumero(value); //la propiedad la pasa a la validacion
+            }
+         }
+    #endregion
+
+    #region Constructores
+
+        public Numero(double numero) //constructo parametrizado
+        {
+            this._Numero = numero;
+            
+        }
+
+        public Numero():this(0) //reutilizo el constructor, this 0
+        {
+           
+        }
+
+        public Numero(string strNumero) //si le paso un string
+        {
+            this.SetNumero = strNumero; //va a la propiedad 
+        }
+
+        #endregion
+
+    #region Metodos  //funciones
+
+        private static double ValidarNumero(string Numero) //la validacion toma un string y devuelve un doble
+            {
+                double num;
+
+                if (double.TryParse(Numero, out num)) //entra Numero string //sale num doble
+                {
+                    return num;
+                }
+
+                return 0;
+            }
+
+        public static string BinarioDecimal(string binario)
+        {
+            double retorno = 0;
+            int j = binario.Length;
+
+            if (binario is null || binario == "") //validacion de NULL o Vacio
+            {
+                return "Error";
+            }
+            else
+            {
+
+                for (int i = 0; i < binario.Length; i++) //Validacion de caracteres invalidos
+                {
+                    if (binario[i] != '0' || binario[i] != '0')
+                    {
+                        return "Error";
+                    }
+                }
+                                             
+                for (int i = 0; i < binario.Length; i++) 
+                {
+                    if (binario[i] == 1)// cada elemento del array es 1
+                    {
+                        retorno += Math.Pow(2, j); //suma la potencia de 2 y 
+                    }
+                    j--;
+
+                }
+                return string.Format("{0}", retorno);
+            }
+                                         
+        }
+
+
+
+        public static string DecimalBinario(double numero)
+        {
+            string binario = "";
+
+                if (numero < 0)
+                {
+                    return null;
+                }
+                while (numero > 0)
+                {
+
+                    if (numero % 2 == 1)
+                    {
+                        binario = '1' + binario;
+                    }
+                    else
+                    {
+                        binario = '0' + binario;
+                    }
+
+                    numero = (int)numero / 2;
+
+                }
+            return binario;
+        }
+
+
+        public static string DecimalBinario(string numero)
+        {
+            double Numero;
+               Numero = ValidarNumero(numero);
+            if (Numero!=0)
+               return DecimalBinario(Numero);
+            else
+              return "ERROR";
+        }
+        
+        #endregion
+
+        #region Sobrecarga de Operadores
+
+        public static double operator +(Numero num1, Numero num2)
+        {
+            double aux = 0;
+
+            aux = num1._Numero+ num2._Numero;          
+
+            return aux;
+        }
+
+        public static double operator -(Numero num1, Numero num2)
+        {
+            ///validaciones de null para todes
+
+            double aux = 0;
+
+            aux = num1._Numero - num2._Numero;
+
+            return aux;
+        }
+
+        public static double operator *(Numero num1, Numero num2)
+        {
+            ///validaciones de null para todes
+
+            double aux = 0;
+
+            aux = num1._Numero - num2._Numero;
+
+            return aux;
+        }
+
+        public static double operator /(Numero num1, Numero num2)
+        {
+            ///validaciones de null para todes
+
+            double aux = 0;
+
+            if (num1._Numero!=0)
+            {
+                aux = num1._Numero / num2._Numero;
+            }            
+
+            return aux;
+        }
+       
+
+        #endregion
+
+    }
+}
